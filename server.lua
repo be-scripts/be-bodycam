f0f80353084404334aa45db31c900e84780658b9
be-scripts <103848882+be-scripts@users.noreply.github.com>
2023-01-14 16:54:53 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,2 +1,53 @@
-print("Hi")
-print("Hi2")
+Core = exports[''..Config.Core]:GetCoreObject()
+
+ RegisterServerEvent("be-bodycam:add")
+ AddEventHandler("be-bodycam:add", function(playerId)
+     local data = {}
+
+     for k,v in pairs(Core.Functions.GetPlayers()) do
+         local xPlayer = Core.Functions.GetPlayer(v)
+
+          local name = xPlayer.PlayerData.charinfo.firstname .. " " .. xPlayer.PlayerData.charinfo.lastname
+          local grade = xPlayer.PlayerData.job.grade.name
+          local jobName = xPlayer.PlayerData.job.name
+
+          local item = xPlayer.Functions.GetItemByName(Config.Item)
+          if item ~= nil and item.amount > 0 then
+                if jobName == "police" then
+                        table.insert(data, {
+                                src = v,
+                                name = name,
+                                grade = grade,
+                        })
+                end
+          end
+
+
+     end
+     TriggerClientEvent("be-bodycam:update", playerId, data)
+ end)
+
+
+Core.Functions.CreateUseableItem(""..Config.Item, function(source, item)
+        local src = source
+        local xPlayer = Core.Functions.GetPlayer(src)
+        local isallowed = false
+        if xPlayer.Functions.GetItemByName(item.name) then
+                local grade = xPlayer.PlayerData.job.grade.level
+                local jobName = xPlayer.PlayerData.job.name
+                if jobName == "police" then
+                        for k,i in pairs(Config.GradeAllowed) do
+                                if(grade == i) then
+                                        TriggerClientEvent("be-bodycam:open", source, source)
+                                        isallowed = true
+                                end
+                        end
+                        if isallowed == false then
+                                TriggerClientEvent(Config.CoreName..':Notify', source, "You must to have specific police grade", 'error', 1500)
+                        end
+                else
+                        TriggerClientEvent(Config.CoreName..':Notify', source, "You must to have police job", 'error', 1500)
+                end
+            end
+    end)
+    
